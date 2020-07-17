bd85babe1b0e71b8784ae10367ae47833c601b6b
Kunal Kumar Verma <ninja.kunal01@gmail.com>
2020-07-17 21:22:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import pyautogui
-import webbrowser
 import time
+import pyautogui
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 driver=webdriver.Chrome('./chromedriver')
@@ -12,26 +11,34 @@ input("Press any key after logging in your whatsapp web account")
 while True:
     name=input("Enter the user name:")
     spam_message=input("Enter your spam message:")
-    frequency=int(input("How many times do you want to send the message:"))
+    try:
+        frequency=int(input("How many times do you want to send the message:"))
+    except ValueError:
+        frequency=5    
     response=input("Response accordingly to initiate spamming (y/n):")
-    user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+    try:
+        user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+    except:
+        print("User not found :(")
+        break
     user.click()
     time.sleep(2)
     msgfield = driver.find_element_by_class_name("_3uMse")
     c=0
+    flag=0
     if response=='y':
         print("Spamming initiated...")
-        for c in range(0,frequency):
+        for c in range(1,frequency+1):
             try:
                 msgfield.send_keys(spam_message)
-                # pyautogui.press('ENTER')
                 time.sleep(0.5)
                 msgfield.send_keys(Keys.ENTER)
-                time.sleep(2)
             except:
                 print("Some error occured :(") 
+                flag=1
                 break
-        print("Spamming completed successfully")
+        if flag==0:
+            print("Spamming completed successfully")
     else:
         print("Meet you next time :)")
         break
